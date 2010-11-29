fe4740981937d3fd79d9fa317464b48fefbccf18
Alosh Bennett <aloshbennett@yahoo.com>
2010-11-30 01:22:12 +0530

    replaced script jsonp with ioservice json

--- chrome/agbshorturl/content/main.js
+++ chrome/agbshorturl/content/main.js
@@ -2,31 +2,6 @@ if ("undefined" == typeof(AGBShortURLChrome)) {
   var AGBShortURLChrome = {};
 };
 
-AGBShortURLChrome.ArrayList = {
-  list : new Array(),
-
-  addToList : function(object) {
-    AGBShortURLChrome.ArrayList.list.push(object);
-  },
-
-  isPresent : function(object) {
-    for(i=0; i<AGBShortURLChrome.ArrayList.list.length; i++) {
-        if(AGBShortURLChrome.ArrayList.list[i] == object)
-            return true;
-    }
-    return false;
-  },
-
-  removeFromList : function(object) {
-    for(i=0; i<AGBShortURLChrome.ArrayList.list.length; i++) {
-        if(AGBShortURLChrome.ArrayList.list[i] == object) {
-            AGBShortURLChrome.ArrayList.list.splice(i, 1);
-            break;
-        }
-    }
-  }
-};
-
 AGBShortURLChrome.Cache = {
   map : new Array(),
 
@@ -53,8 +28,6 @@ AGBShortURLChrome.Cache = {
 
 
 AGBShortURLChrome.Shortly = {
-  callList : AGBShortURLChrome.ArrayList,
-
   urlCache : AGBShortURLChrome.Cache,
 
   requestShortURL : function(aEvent) {
@@ -66,34 +39,41 @@ AGBShortURLChrome.Shortly = {
   },
 
   shortenURL : function(url) {
-    AGBShortURLChrome.Shortly.callList.addToList(url);
     url = encodeURIComponent(url);
-    script = window.content.document.createElement('script');
-    script.type = 'text/javascript';
-      function agbshorturlchrome_shortly_callback(result) {
-        var dataNode = document.createElement('AGBShortURLChromeDataNode');
-        dataNode.setAttribute('longURL', document.URL);
-        dataNode.setAttribute('shortURL', result.data.url);
-        document.documentElement.appendChild(dataNode);
-        var event = document.createEvent('Event');
-        event.initEvent('AGBShortURLChrome.shortly.callbackEvent', true, false);
-        dataNode.dispatchEvent(event);
-      }
-    scriptCode = window.content.document.createTextNode(agbshorturlchrome_shortly_callback);
-    script.appendChild(scriptCode);
-    window.content.document.body.appendChild(script);
-    script = window.content.document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'http://api.bit.ly/v3/shorten?login=oneclickshorten&apiKey=R_66e801e8a9a088ded55f075f78eb7d90&longUrl='+url+'&format=json&callback=agbshorturlchrome_shortly_callback';
-    window.content.document.body.appendChild(script);
+    var ioService=Components.classes["@mozilla.org/network/io-service;1"]
+        .getService(Components.interfaces.nsIIOService);
+    var scriptableStream=Components
+        .classes["@mozilla.org/scriptableinputstream;1"]
+        .getService(Components.interfaces.nsIScriptableInputStream);
+    var bitlyURL = 'http://api.bit.ly/v3/shorten?login=oneclickshorten&apiKey=R_66e801e8a9a088ded55f075f78eb7d90&longUrl='+url+'&format=json';
+    var channel=ioService.newChannel(bitlyURL,null,null);
+    function ServiceListener(callback) {
+        this.callback = callback;
+    }
+
+    ServiceListener.prototype = {
+       serviceData : "",
+       onStartRequest: function(request, ctx) {
+       },
+
+       onDataAvailable : function(request, context, inputStream, offset, count) {
+        scriptableStream.init(inputStream);
+        this.serviceData += scriptableStream.read(count);
+       },
+
+       onStopRequest: function(request, ctx, status) {
+        this.callback(JSON.parse(this.serviceData));
+       }
+    }
+    var listener = new ServiceListener(AGBShortURLChrome.Shortly.callbackHandler);
+    //channel.notificationCallbacks = listener;
+    channel.asyncOpen(listener, null);
+
   },
 
-  callbackEventHandler : function(event) {
-    let shortURL = event.target.getAttribute('shortURL');
-    let longURL = event.target.getAttribute('longURL');
-    if(!AGBShortURLChrome.Shortly.callList.isPresent(longURL))
-        return;
-    AGBShortURLChrome.Shortly.callList.removeFromList(longURL);
+  callbackHandler : function(result) {
+    let shortURL = result.data.url;
+    let longURL = result.data.long_url;
     AGBShortURLChrome.Shortly.urlCache.addToCache(longURL, shortURL);
     AGBShortURLChrome.Shortly.displayShortURL(shortURL);
   },
@@ -106,10 +86,11 @@ AGBShortURLChrome.Shortly = {
   displayShortURL : function(shortURL) {
     let shortURLBar = document.getElementById("agbshorturl-statusbar-urltext");
     if(shortURL == null) {
+        shortURLBar.hidden=true;
         shortURLBar.value = "";
-        shortURLBar.size = 1;
     }
     else {   
+        shortURLBar.hidden=false;
         shortURLBar.value = shortURL;
         shortURLBar.size = shortURL.length-3;
     }
@@ -117,7 +98,7 @@ AGBShortURLChrome.Shortly = {
 
 };
 
-window.addEventListener('AGBShortURLChrome.shortly.callbackEvent', AGBShortURLChrome.Shortly.callbackEventHandler, false, true);
 window.gBrowser.tabContainer.addEventListener('TabSelect', AGBShortURLChrome.Shortly.resetDisplay, false, true);
+window.gBrowser.addEventListener('pageshow', AGBShortURLChrome.Shortly.resetDisplay, false);
 
 
