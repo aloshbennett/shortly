5e72b6230e4ebe7dd6ed58dfd1c0592f2b7c1cdf
Alosh Bennett <aloshbennett@gmail.com>
2014-07-12 15:53:33 +0530

    German translation

--- defaults/preferences/preferences.js
+++ defaults/preferences/preferences.js
@@ -2,6 +2,6 @@ pref("extensions.shortly.contextmenu.enable", true);
 pref("extensions.shortly.urlbox.enable", true);
 pref("extensions.shortly.addonbar.enable", true);
 pref("extensions.shortly.accesskey.enable", true);
-pref("extensions.shortly.accesskey.combination", "CTRL-ALT-S");
+pref("extensions.shortly.accesskey.combination", "CTRL-ALT-I");
 pref("extensions.shortly.clipboard.enable", true);
 
