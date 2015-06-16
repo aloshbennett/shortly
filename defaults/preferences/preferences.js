eb4e521a0981d20d0eb3bb5306bb651dbafbcb02
Alosh Bennett <aloshbennett@gmail.com>
2015-06-16 20:20:24 +0530

    changed default key

--- defaults/preferences/preferences.js
+++ defaults/preferences/preferences.js
@@ -4,5 +4,5 @@ pref("extensions.shortly.addonbar.enable", true);
 pref("extensions.shortly.accesskey.enable", true);
 pref("extensions.shortly.accesskey.combination", "CTRL-ALT-I");
 pref("extensions.shortly.clipboard.enable", true);
-pref("extensions.shortly.key", "a76af0278eea68024e432cc074689ba76c9259ce");
+pref("extensions.shortly.key", "b48b63d7123ea5fe4d07c99d1f66562fef73413b");
 pref("extensions.shortly.key.expiry", "0");
